204c83574888d5dc506d469922feded8a8dbfc99
EvgeniyIvankin <6733480@gmail.com>
2018-05-28 23:14:27 +0300

    Create life search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ArtistService } from './artist.service';
+import { Observable, Subject } from 'rxjs';
+import { Artist } from './Artist';
+import {
+  debounceTime, distinctUntilChanged, switchMap
+} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  name: string;
   title = 'app';
+  artists$: Observable<Artist[]>;
+  private searchTerms = new Subject<string>();
+
+  constructor(private artistService: ArtistService) {}
+
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+  ngOnInit() {
+    this.artists$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string) => this.artistService.searchArtists(term)),
+    );
+  }
+
+  open() {
+    console.log(this.name);
+  }
+
 }
--- src/app/artist.service.ts
+++ src/app/artist.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Artist } from './Artist';
-import { Observable } from 'rxjs/internal/Observable';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { MessageService } from './message.service';
 
 
 @Injectable()
@@ -14,14 +16,24 @@ export class ArtistService {
                          '&api_key=' + this.key + '&format=json';
 
   constructor(
-    private http: HttpClient) { }
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
-  searchArtists(term) {
-    return this.http.get('http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=' + term + '&api_key='
-    + this.key + '&format=json&limit=5');
+  searchArtists(term): Observable<Artist[]> {
+    if (!term.trim()) {
+      return of([]);
+    }
+    return this.http.get<Artist[]>('http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=' + term + '&api_key=' +
+    this.key + '&format=json&limit=5')
+    .pipe(map(result => {
+        const artistsList = result['results'].artistmatches.artist;
+          return artistsList.map(artist => {
+            return { name: artist.name, image: artist.image[0]['#text'] };
+          });
+      }));
   }
 
-  getTopArtists () {
+  getTopArtists() {
     return this.http.get(this.getTopArtistsURL);
   }
 
@@ -40,6 +52,31 @@ export class ArtistService {
   getFullInfoAboutAlbum(artistName, albumName) {
     return this.http.get('http://ws.audioscrobbler.com/2.0/?method=album.getinfo&artist=' + artistName +
     '&api_key=' + this.key + '&album=' + albumName + '&format=json');
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result ?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add('HeroService: ' + message);
   }
 
 }
